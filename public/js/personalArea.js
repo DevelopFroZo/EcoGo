26efb5893b90642c8129eb08b424cedd28259b6d
Max Zhukov <promakc31@gmail.com>
2019-09-29 00:14:29 +0800

    add user status in index.html

--- public/js/personalArea.js
+++ public/js/personalArea.js
@@ -1,3 +1,19 @@
-$(document).ready(function () {
+let requests = new Requests({
+    dataType: "json",
+    responsePreprocess: data => JSON.parse(data)
+});
+
+$(document).ready(async function () {
     initBurger();
+
+    let data = await requests.post("/users/getUserInfo");
+
+    if (data.isSuccess) {
+        $("#qrCode").attr("src", data.qrcode)
+        $("#user-name").text(data.fi);
+        $("#user-mail").text(data.email);
+        $("#user-number").text(data.phone).attr("href", "tel:" + data.phone);
+        $(".tokens").text(data.balance + " баллов")
+    }
+
 })
